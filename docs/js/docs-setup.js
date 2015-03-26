4cdd3c25bcbe0a20857e52f036314b98fb6be7c9
Severin Burgstaller <seb@westbahn.at>
2015-03-26 17:36:37 +0100

    docs update in controller

---
+++ docs/js/docs-setup.js
@@ -0,0 +1,141 @@
+NG_DOCS={
+  "sections": {
+    "api": "API Documentation"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "sbc.controllers",
+      "shortName": "sbc.controllers",
+      "type": "overview",
+      "moduleName": "sbconverter",
+      "shortDescription": "Controller of MVC pattern for sbconverter",
+      "keywords": "api controller controllers mvc overview pattern sbc sbconverter"
+    },
+    {
+      "section": "api",
+      "id": "sbc.controllers:ConversionSelectionCtrl",
+      "shortName": "ConversionSelectionCtrl",
+      "type": "controller",
+      "moduleName": "sbc",
+      "shortDescription": "Root controller which handels API calls and data handling for conversion and conversion itself",
+      "keywords": "$scope api base calls controller controllers conversion convert currencies currency data handels handling method queries root sbc service update updatecurrency updates"
+    },
+    {
+      "section": "api",
+      "id": "sbc.services",
+      "shortName": "sbc.services",
+      "type": "overview",
+      "moduleName": "sbconverter",
+      "shortDescription": "API services for sbconverter.",
+      "keywords": "api overview sbc sbconverter services"
+    },
+    {
+      "section": "api",
+      "id": "sbc.services.API:getRatesURL",
+      "shortName": "getRatesURL",
+      "type": "service",
+      "moduleName": "sbc.services.API",
+      "shortDescription": "Provides access to API URL",
+      "keywords": "access api sbc service services url"
+    },
+    {
+      "section": "api",
+      "id": "sbc.services.Currencies:convert",
+      "shortName": "convert",
+      "type": "service",
+      "moduleName": "Currencies",
+      "shortDescription": "Converts currencies",
+      "keywords": "api base basecurrency basevalue convert converts currencies currency sbc service services target targetcurrency"
+    },
+    {
+      "section": "api",
+      "id": "sbc.services.Currencies:getCurrencies",
+      "shortName": "getCurrencies",
+      "type": "service",
+      "moduleName": "Currencies",
+      "shortDescription": "Load all convertable currencies",
+      "keywords": "api convertable currencies load sbc service services"
+    },
+    {
+      "section": "api",
+      "id": "sbc.services.Fixr:getExchangeRates",
+      "shortName": "getExchangeRates",
+      "type": "service",
+      "moduleName": "Fixr",
+      "shortDescription": "Load exchange rates",
+      "keywords": "api base basecurrency currency exchange fixr load query rates sbc service services"
+    },
+    {
+      "section": "api",
+      "id": "sbc.services:API",
+      "shortName": "API",
+      "type": "overview",
+      "moduleName": "sbc.services",
+      "shortDescription": "Base API services for sbconverter.",
+      "keywords": "api base overview sbc sbconverter services"
+    },
+    {
+      "section": "api",
+      "id": "sbc.services:Currencies",
+      "shortName": "Currencies",
+      "type": "overview",
+      "moduleName": "Currencies",
+      "shortDescription": "Currency conversion implementation (based on Fixr API)",
+      "keywords": "$http $q api conversion currencies currency fixr implementation overview sbc services"
+    },
+    {
+      "section": "api",
+      "id": "sbc.services:Fixr",
+      "shortName": "Fixr",
+      "type": "overview",
+      "moduleName": "Fixr",
+      "shortDescription": "Fixr API implementation (needs API.getRatesURL)",
+      "keywords": "$http $q api fixr getratesurl implementation overview sbc services"
+    },
+    {
+      "section": "api",
+      "id": "sbconverter",
+      "shortName": "sbconverter",
+      "type": "overview",
+      "moduleName": "sbconverter",
+      "shortDescription": "Represents the main application &quot;sbconverter&quot;.",
+      "keywords": "api application controllers main overview represents sbc sbconverter services"
+    }
+  ],
+  "apis": {
+    "api": true
+  },
+  "__file": "_FAKE_DEST_/js/docs-setup.js",
+  "__options": {
+    "startPage": "/api",
+    "scripts": [
+      "js/angular.min.js",
+      "js/angular-animate.min.js",
+      "js/marked.js",
+      "js/prettify.js"
+    ],
+    "styles": [],
+    "title": "API Documentation",
+    "html5Mode": true,
+    "editExample": true,
+    "navTemplate": false,
+    "navContent": "",
+    "navTemplateData": {},
+    "loadDefaults": {
+      "angular": true,
+      "angularAnimate": true,
+      "marked": true,
+      "prettify": true
+    }
+  },
+  "html5Mode": true,
+  "editExample": true,
+  "startPage": "/api",
+  "scripts": [
+    "angular.min.js",
+    "angular-animate.min.js",
+    "marked.js",
+    "prettify.js"
+  ]
+};
