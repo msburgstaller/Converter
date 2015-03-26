f06c0e9f845b77434a19e3f13eed7d1cc061cc8a
Severin Burgstaller <seb@westbahn.at>
2015-03-26 17:30:19 +0100

    docs

--- gulpfile.js
+++ gulpfile.js
@@ -11,3 +11,11 @@ gulp.task('default', function ()
 {
     // place code for your default task here
 });
+
+gulp.task('docs', [], function ()
+{
+    var gulpDocs = require('gulp-ngdocs');
+    return gulp.src('www/js/*.js')
+            .pipe(gulpDocs.process())
+            .pipe(gulp.dest('./docs'));
+});
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,12 +1,22 @@
 /**
- *
- *
- * @file:
- * @author: Severin Burgstaller
+ * @ngdoc overview
+ * @name sbc.controllers
+ * @module sbconverter
+ * @description
+ * Controller of MVC pattern for sbconverter
  */
-
 angular.module("sbc.controllers", [])
 
+/**
+ * @ngdoc controller
+ * @name sbc.controllers:ConversionSelectionCtrl
+ *
+ * @requires $scope
+ * @requires Currencies
+ *
+ * @description
+ * Root controller which handels API calls and data handling for conversion and conversion itself
+ */
         .controller('ConversionSelectionCtrl', function ($scope, Currencies)
         {
 
@@ -30,6 +40,18 @@ angular.module("sbc.controllers", [])
                 $scope.convert();
             });
 
+            /**
+             * @ngdoc method
+             * @name sbc.controllers:ConversionSelectionCtrlh#updateCurrency
+             *
+             * @param {string} to which (target or base) to update
+             * @param {string} currency with which currency to update
+             *
+             * @methodOf sbc.controllers:ConversionSelectionCtrl
+             *
+             * @description
+             * Updates currency for conversion in $scope.conversion
+             */
             $scope.updateCurrency = function (to, currency)
             {
                 switch (to)
@@ -49,6 +71,15 @@ angular.module("sbc.controllers", [])
                 $scope.convert();
             };
 
+            /**
+             * @ngdoc method
+             * @name sbc.controllers:ConversionSelectionCtrlh#convert
+             *
+             * @methodOf sbc.controllers:ConversionSelectionCtrl
+             *
+             * @description
+             * Queries conversion service and updates $scope.conversion
+             */
             $scope.convert = function ()
             {
                 Currencies
