55a9ade7d12d6c5958ca255739562f7e60d1b503
Severin Burgstaller <seb@westbahn.at>
2015-04-10 08:59:48 +0200

    migrated to ionic

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,50 @@
-/**
- *
- *
- * @file:
- * @author: Severin Burgstaller
- */
-
 var gulp = require('gulp');
-var serve = require('gulp-serve');
+var gutil = require('gulp-util');
+var bower = require('bower');
+var concat = require('gulp-concat');
+var sass = require('gulp-sass');
+var minifyCss = require('gulp-minify-css');
+var rename = require('gulp-rename');
+var sh = require('shelljs');
 
-gulp.task('default', function ()
-{
-    // place code for your default task here
+var paths = {
+  sass: ['./scss/**/*.scss']
+};
+
+gulp.task('default', ['sass']);
+
+gulp.task('sass', function(done) {
+  gulp.src('./scss/ionic.app.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('./www/css/'))
+    .pipe(minifyCss({
+      keepSpecialComments: 0
+    }))
+    .pipe(rename({ extname: '.min.css' }))
+    .pipe(gulp.dest('./www/css/'))
+    .on('end', done);
 });
 
-gulp.task('docs', [], function ()
-{
-    var gulpDocs = require('gulp-ngdocs');
-    return gulp.src('www/js/*.js')
-            .pipe(gulpDocs.process())
-            .pipe(gulp.dest('./docs'));
+gulp.task('watch', function() {
+  gulp.watch(paths.sass, ['sass']);
 });
 
-//gulp.task('serve', serve('public'));
-//gulp.task('serve-docs', serve(['public', 'build']));
-gulp.task('serve-docs', serve({
-    root: ['docs'],
-    port: 8005
-}));
+gulp.task('install', ['git-check'], function() {
+  return bower.commands.install()
+    .on('log', function(data) {
+      gutil.log('bower', gutil.colors.cyan(data.id), data.message);
+    });
+});
+
+gulp.task('git-check', function(done) {
+  if (!sh.which('git')) {
+    console.log(
+      '  ' + gutil.colors.red('Git is not installed.'),
+      '\n  Git, the version control system, is required to download Ionic.',
+      '\n  Download git here:', gutil.colors.cyan('http://git-scm.com/downloads') + '.',
+      '\n  Once git is installed, run \'' + gutil.colors.cyan('gulp install') + '\' again.'
+    );
+    process.exit(1);
+  }
+  done();
+});
---
+++ old/www/js/services.js
@@ -0,0 +1,200 @@
+/**
+ * @ngdoc overview
+ * @name sbc.services
+ * @module sbc.services
+ * @description
+ * API services for sbconverter.
+ */
+angular.module("sbc.services", [])
+
+        /**
+         * @ngdoc overview
+         * @name sbc.services:API
+         * @module sbc.services
+         *
+         * @description
+         * Base API services for sbconverter.
+         */
+        .factory("API", function ()
+        {
+            var baseURL = 'https://api.fixer.io/';
+
+            /**
+             * @ngdoc service
+             * @name sbc.services.API:getRatesURL
+             * @module sbc.services.API
+             *
+             * @description Provides access to API URL
+             */
+            this.getRatesURL = function (base, from, to, date)
+            {
+                from = from || false;
+                base = base || false;
+                to = to || false;
+                date = date || 'latest';
+
+                var returnURL = baseURL + date;
+
+                if (date !== false)
+                {
+
+                }
+
+                if (base !== false)
+                {
+                    returnURL += '?base=' + base
+                }
+
+                if (from !== false)
+                {
+                    returnURL += '?symbols=' + from
+                }
+
+                if (from !== false && to !== false)
+                {
+                    returnURL += ',' + to
+                }
+
+                return returnURL
+            };
+
+            return this;
+        })
+
+        /**
+         * @ngdoc overview
+         * @name sbc.services:Fixr
+         * @module sbc.services
+         *
+         * @requires API
+         * @requires $http
+         * @requires $q
+         *
+         * @description
+         * Fixr API implementation (needs API.getRatesURL)
+         */
+        .factory("Fixr", function (API, $http, $q)
+        {
+            var currencies = {};
+
+            return {
+                /**
+                 * @ngdoc service
+                 * @name sbc.services.Fixr:getExchangeRates
+                 * @module Fixr
+                 * @description Load exchange rates
+                 * @param {string} baseCurrency base currency to query for
+                 */
+                getExchangeRates: function (baseCurrency)
+                {
+                    var deferred = $q.defer();
+
+                    if (typeof currencies[baseCurrency] !== 'undefined')
+                    {
+                        deferred.resolve(currencies[baseCurrency]);
+                    }
+                    else
+                    {
+                        $http.get(API.getRatesURL(baseCurrency))
+                                .success(function (data, status, headers, config)
+                                {
+                                    currencies = data;
+
+                                    deferred.resolve(currencies);
+                                });
+                    }
+
+                    return deferred.promise;
+                }
+            };
+
+
+        })
+
+        /**
+         * @ngdoc overview
+         * @name sbc.services:Currencies
+         * @module sbc.services
+         *
+         * @requires Fixr
+         * @requires $http
+         * @requires $q
+         *
+         * @description
+         * Currency conversion implementation (based on Fixr API)
+         */
+        .factory("Currencies", function (Fixr, $http, $q)
+        {
+            return {
+                /**
+                 * @ngdoc service
+                 * @name sbc.services.Currencies:convert
+                 * @description Converts currencies
+                 *
+                 * @param {number} baseValue base value from which to convert
+                 * @param {string} targetCurrency target currency to convert to
+                 * @param {string} baseCurrency base currency to convert from
+                 */
+                convert: function (baseValue, targetCurrency, baseCurrency)
+                {
+                    baseCurrency = baseCurrency || 'EUR';
+
+                    if (typeof baseValue === 'undefined') throw "Currencies.convert: Base value missing.";
+                    if (typeof targetCurrency === 'undefined') throw "Currencies.convert: Target currency missing.";
+
+                    var deferred = $q.defer();
+
+                    Fixr.getExchangeRates(baseCurrency).then(function (data)
+                    {
+                        var ret = {
+                            rate: null,
+                            baseValue: baseValue,
+                            baseCurrency: null,
+                            targetValue: null,
+                            targetCurrency: targetCurrency
+                        };
+
+                        if (data.base != targetCurrency)
+                        {
+                            ret.rate = data.rates[targetCurrency];
+                            ret.targetValue = ret.rate * baseValue;
+                            ret.baseCurrency = baseCurrency;
+                        }
+                        else
+                        {
+                            ret.rate = 1;
+                            ret.baseCurrency = targetCurrency;
+                            ret.targetValue = baseValue;
+                        }
+
+                        deferred.resolve(ret);
+                    });
+
+                    return deferred.promise;
+                },
+
+                /**
+                 * @ngdoc service
+                 * @name sbc.services.Currencies:getCurrencies
+                 * @description Load all convertable currencies
+                 */
+                getCurrencies: function ()
+                {
+                    var deferred = $q.defer();
+
+                    Fixr.getExchangeRates().then(function (data)
+                    {
+                        var ret = [data.base];
+
+                        for (var i in data.rates)
+                        {
+                            ret[ret.length] = i;
+                        }
+
+                        deferred.resolve(ret);
+                    });
+
+                    return deferred.promise;
+                }
+            };
+        });
